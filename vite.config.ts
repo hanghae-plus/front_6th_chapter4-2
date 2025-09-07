40d72e6797a5b1f8223b880cd3df2a2938d3f27f
junilhwang <junil.h@kakao.com>
2025-09-07 22:44:25 +0900

    fix: 배포 했을 때 정상동작 하도록 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-oxc";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
-      },
-    },
-  }),
-);
+export default defineConfig({
+  plugins: [react()],
+  base: "/front_6th_chapter4-2/",
+});
