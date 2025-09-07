2b4ecc118b10e86c36deed33c7a2bdc814497bf6
areumH <dkfmal0109@gmail.com>
2025-09-08 03:27:51 +0900

    chore: 전체 주석 추가

--- src/ScheduleDndProvider.tsx
+++ src/ScheduleDndProvider.tsx
@@ -1,8 +1,10 @@
-import { DndContext, Modifier, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { PropsWithChildren } from "react";
+import { DndContext, Modifier, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { CellSize, DAY_LABELS } from "./constants.ts";
 import { useScheduleContext } from "./ScheduleContext.tsx";
 
+// 드래그 스냅 함수 
+// 그리드에 맞춰 강의 스냅, 컨테이너 밖을 나가지 않도록 제한
 function createSnapModifier(): Modifier {
   return ({ transform, containerNodeRect, draggingNodeRect }) => {
     const containerTop = containerNodeRect?.top ?? 0;
@@ -31,20 +33,27 @@ const modifiers = [createSnapModifier()]
 export default function ScheduleDndProvider({ children }: PropsWithChildren) {
   const { schedulesMap, setSchedulesMap } = useScheduleContext();
   const sensors = useSensors(
+    // PointerSensor - 마우스나 터치 입력을 기준으로 드래그 감지
     useSensor(PointerSensor, {
       activationConstraint: {
-        distance: 8,
+        distance: 8, // 8px 이상 움직여야 드래그 시작
       },
     })
   );
 
+  // 드래그 종료 시 호출
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleDragEnd = (event: any) => {
     const { active, delta } = event;
+    // active - 드래그한 아이템
+    // delta - 이동 거리 
+
     const { x, y } = delta;
     const [tableId, index] = active.id.split(':');
     const schedule = schedulesMap[tableId][index];
     const nowDayIndex = DAY_LABELS.indexOf(schedule.day as typeof DAY_LABELS[number])
+
+    // 이동한 그리드 계산
     const moveDayIndex = Math.floor(x / 80);
     const moveTimeIndex = Math.floor(y / 30);
 
@@ -52,7 +61,7 @@ export default function ScheduleDndProvider({ children }: PropsWithChildren) {
       ...schedulesMap,
       [tableId]: schedulesMap[tableId].map((targetSchedule, targetIndex) => {
         if (targetIndex !== Number(index)) {
-          return { ...targetSchedule }
+          return { ...targetSchedule } // 다른 강의는 그대로 유지
         }
         return {
           ...targetSchedule,
--- src/ScheduleTables.tsx
+++ src/ScheduleTables.tsx
@@ -1,61 +1,81 @@
-import { Button, ButtonGroup, Flex, Heading, Stack } from "@chakra-ui/react";
-import ScheduleTable from "./ScheduleTable.tsx";
-import { useScheduleContext } from "./ScheduleContext.tsx";
-import SearchDialog from "./SearchDialog.tsx";
-import { useState } from "react";
+import { useState } from 'react';
+import { Button, ButtonGroup, Flex, Heading, Stack } from '@chakra-ui/react';
+import { useScheduleContext } from './ScheduleContext.tsx';
+import ScheduleTable from './ScheduleTable.tsx';
+import SearchDialog from './SearchDialog.tsx';
 
 export const ScheduleTables = () => {
-  const { schedulesMap, setSchedulesMap } = useScheduleContext();
-  const [searchInfo, setSearchInfo] = useState<{
-    tableId: string;
-    day?: string;
-    time?: number;
-  } | null>(null);
+  // 시간표 배열
+	const { schedulesMap, setSchedulesMap } = useScheduleContext();
 
-  const disabledRemoveButton = Object.keys(schedulesMap).length === 1;
+  // 현재 선택된 강의 정보 - 시간표 id, 요일, 시간
+	const [searchInfo, setSearchInfo] = useState<{
+		tableId: string;
+		day?: string;
+		time?: number;
+	} | null>(null);
 
-  const duplicate = (targetId: string) => {
-    setSchedulesMap(prev => ({
-      ...prev,
-      [`schedule-${Date.now()}`]: [...prev[targetId]]
-    }))
-  };
+  // schedulesMap의 길이가 1일 때 삭제 불가능 처리 - 최소 시간표가 하나는 있어야 한다?
+	const disabledRemoveButton = Object.keys(schedulesMap).length === 1;
 
-  const remove = (targetId: string) => {
-    setSchedulesMap(prev => {
-      delete prev[targetId];
-      return { ...prev };
-    })
-  };
+  // 시간표 복제 - schedulesMap의 키 값을 인자로 받아 시간표 복제
+	const duplicate = (targetId: string) => {
+		setSchedulesMap((prev) => ({
+			...prev,
+			[`schedule-${Date.now()}`]: [...prev[targetId]],
+		}));
+	};
 
-  return (
-    <>
-      <Flex w="full" gap={6} p={6} flexWrap="wrap">
-        {Object.entries(schedulesMap).map(([tableId, schedules], index) => (
-          <Stack key={tableId} width="600px">
-            <Flex justifyContent="space-between" alignItems="center">
-              <Heading as="h3" fontSize="lg">시간표 {index + 1}</Heading>
-              <ButtonGroup size="sm" isAttached>
-                <Button colorScheme="green" onClick={() => setSearchInfo({ tableId })}>시간표 추가</Button>
-                <Button colorScheme="green" mx="1px" onClick={() => duplicate(tableId)}>복제</Button>
-                <Button colorScheme="green" isDisabled={disabledRemoveButton}
-                        onClick={() => remove(tableId)}>삭제</Button>
-              </ButtonGroup>
-            </Flex>
-            <ScheduleTable
-              key={`schedule-table-${index}`}
-              schedules={schedules}
-              tableId={tableId}
-              onScheduleTimeClick={(timeInfo) => setSearchInfo({ tableId, ...timeInfo })}
-              onDeleteButtonClick={({ day, time }) => setSchedulesMap((prev) => ({
-                ...prev,
-                [tableId]: prev[tableId].filter(schedule => schedule.day !== day || !schedule.range.includes(time))
-              }))}
-            />
-          </Stack>
-        ))}
-      </Flex>
-      <SearchDialog searchInfo={searchInfo} onClose={() => setSearchInfo(null)}/>
-    </>
-  );
-}
+  // 시간표 삭제 - schedulesMap의 키 값을 인자로 받아 시간표 삭제`₩
+	const remove = (targetId: string) => {
+		setSchedulesMap((prev) => {
+			delete prev[targetId];
+			return { ...prev };
+		});
+	};
+
+	return (
+		<>
+			<Flex w="full" gap={6} p={6} flexWrap="wrap">
+				{Object.entries(schedulesMap).map(([tableId, schedules], index) => (
+					<Stack key={tableId} width="600px">
+						<Flex justifyContent="space-between" alignItems="center">
+							<Heading as="h3" fontSize="lg">
+								시간표 {index + 1}
+							</Heading>
+							<ButtonGroup size="sm" isAttached>
+								<Button colorScheme="green" onClick={() => setSearchInfo({ tableId })}>
+									시간표 추가
+								</Button>
+								<Button colorScheme="green" mx="1px" onClick={() => duplicate(tableId)}>
+									복제
+								</Button>
+								<Button colorScheme="green" isDisabled={disabledRemoveButton} onClick={() => remove(tableId)}>
+									삭제
+								</Button>
+							</ButtonGroup>
+						</Flex>
+
+						{/* 시간표 컴포넌트 */}
+						<ScheduleTable
+							key={`schedule-table-${index}`}
+							schedules={schedules}
+							tableId={tableId}
+							onScheduleTimeClick={(timeInfo) => setSearchInfo({ tableId, ...timeInfo })}
+							onDeleteButtonClick={({ day, time }) =>
+                // 시간표에서 클릭한 요일과 시간이 일치하는 강의 삭제
+								setSchedulesMap((prev) => ({
+									...prev,
+									[tableId]: prev[tableId].filter((schedule) => schedule.day !== day || !schedule.range.includes(time)),
+								}))
+							}
+						/>
+					</Stack>
+				))}
+			</Flex>
+
+      {/* searchInfo가 null이 아니면 검색 dialog가 보여짐 */}
+			<SearchDialog searchInfo={searchInfo} onClose={() => setSearchInfo(null)} />
+		</>
+	);
+};
