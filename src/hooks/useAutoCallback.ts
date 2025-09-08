cddd64727b14b52ebbc9b5d5586e444a43c6c695
Amelia-Shin <gmldnjs0402@naver.com>
2025-09-08 20:44:07 +0900

    feat: SearchDialog 컴포넌트 최적화 및 메모이제이션 추가

---
+++ src/hooks/useAutoCallback.ts
@@ -0,0 +1,15 @@
+import { useCallback, useRef } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyFunction = (...args: any[]) => any;
+
+export const useAutoCallback = (callback: AnyFunction) => {
+  const ref = useRef(callback);
+
+  ref.current = callback;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return useCallback((...args: any[]) => {
+    ref.current(...args);
+  }, []);
+};
