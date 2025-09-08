2899e348fb19836d210ed9a6ec773988240c44e4
JiHoon-0330 <ljihoon0330@gmail.com>
2025-09-08 20:32:36 +0900

    스트릭모드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<ChakraProvider>
-			<App />
-		</ChakraProvider>
-	</React.StrictMode>,
+createRoot(document.getElementById("root")!).render(
+	<ChakraProvider>
+		<App />
+	</ChakraProvider>,
 );
