22ee5cdb7583ffd3f1ae35259d14dca743101866
Angie Lee <angielxx94@gmail.com>
2025-09-09 23:06:29 +0900

    feat: SearchDialog 1차 최적화 - await 제거

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from "react";
 import {
   Box,
   Button,
@@ -29,11 +28,12 @@ import {
   VStack,
   Wrap,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 import { useScheduleContext } from "./ScheduleContext.tsx";
+import { DAY_LABELS } from "./constants.ts";
 import { Lecture } from "./types.ts";
 import { parseSchedule } from "./utils.ts";
-import axios from "axios";
-import { DAY_LABELS } from "./constants.ts";
 
 interface Props {
   searchInfo: {
@@ -87,12 +87,12 @@ const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.jso
 
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
 const fetchAllLectures = async () => await Promise.all([
-  (console.log('API Call 1', performance.now()), await fetchMajors()),
-  (console.log('API Call 2', performance.now()), await fetchLiberalArts()),
-  (console.log('API Call 3', performance.now()), await fetchMajors()),
-  (console.log('API Call 4', performance.now()), await fetchLiberalArts()),
-  (console.log('API Call 5', performance.now()), await fetchMajors()),
-  (console.log('API Call 6', performance.now()), await fetchLiberalArts()),
+  (console.log('API Call 1', performance.now()), fetchMajors()),
+  (console.log('API Call 2', performance.now()), fetchLiberalArts()),
+  (console.log('API Call 3', performance.now()), fetchMajors()),
+  (console.log('API Call 4', performance.now()), fetchLiberalArts()),
+  (console.log('API Call 5', performance.now()), fetchMajors()),
+  (console.log('API Call 6', performance.now()), fetchLiberalArts()),
 ]);
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
