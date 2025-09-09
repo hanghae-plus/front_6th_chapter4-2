63762746cd661449716266ac3f742cc141664f3f
jihyeon <rachel2148072@gmail.com>
2025-09-10 00:55:29 +0900

    feat: SearchDialog 개선 - 불필요한 연산 발생 조정

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -98,41 +98,52 @@ const SearchDialog = memo(({ searchInfo, onClose }: Props) => {
 
   const filteredLectures = useMemo(() => {
     const { query = "", credits, grades, days, times, majors } = searchOptions;
-    return lectures
-      .filter(
-        (lecture) =>
-          lecture.title.toLowerCase().includes(query.toLowerCase()) ||
-          lecture.id.toLowerCase().includes(query.toLowerCase())
-      )
-      .filter(
-        (lecture) => grades.length === 0 || grades.includes(lecture.grade)
-      )
-      .filter(
-        (lecture) => majors.length === 0 || majors.includes(lecture.major)
-      )
-      .filter(
-        (lecture) => !credits || lecture.credits.startsWith(String(credits))
-      )
-      .filter((lecture) => {
-        if (days.length === 0) {
-          return true;
-        }
+
+    return lectures.filter((lecture) => {
+      if (query) {
+        const lowerQuery = query.toLowerCase();
+        if (
+          !(
+            lecture.title.toLowerCase().includes(lowerQuery) ||
+            lecture.id.toLowerCase().includes(lowerQuery)
+          )
+        )
+          return false;
+      }
+
+      if (grades.length > 0 && !grades.includes(lecture.grade)) {
+        return false;
+      }
+
+      if (majors.length > 0 && !majors.includes(lecture.major)) {
+        return false;
+      }
+
+      if (credits && !lecture.credits.startsWith(String(credits))) {
+        return false;
+      }
+
+      if (days.length > 0 || times.length > 0) {
         const schedules = lecture.schedule
           ? parseSchedule(lecture.schedule)
           : [];
-        return schedules.some((s) => days.includes(s.day));
-      })
-      .filter((lecture) => {
-        if (times.length === 0) {
-          return true;
-        }
-        const schedules = lecture.schedule
-          ? parseSchedule(lecture.schedule)
-          : [];
-        return schedules.some((s) =>
-          s.range.some((time) => times.includes(time))
-        );
-      });
+        if (
+          days.length > 0 &&
+          !schedules.some((schedule) => days.includes(schedule.day))
+        )
+          return false;
+
+        if (
+          times.length > 0 &&
+          !schedules.some((schedule) =>
+            schedule.range.some((time) => times.includes(time))
+          )
+        )
+          return false;
+      }
+
+      return true;
+    });
   }, [lectures, searchOptions]);
 
   const { lastPage, visibleLectures } = useMemo(
