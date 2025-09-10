6650b883b409b60fb4f831f023a75b5d555ed2fc
ldhldh07 <dhlim07@naver.com>
2025-09-10 13:56:46 +0900

    fix: api 중복호출 방지

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -82,15 +82,19 @@ const TIME_SLOTS = [
 
 const PAGE_SIZE = 100;
 
+const allLecturesCache: {
+  majors?: Promise<import("axios").AxiosResponse<Lecture[]>>;
+  liberal?: Promise<import("axios").AxiosResponse<Lecture[]>>;
+} = {};
+
 const fetchMajors = () => axios.get<Lecture[]>("/schedules-majors.json");
 const fetchLiberalArts = () => axios.get<Lecture[]>("/schedules-liberal-arts.json");
 
-// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-const fetchAllLectures = async () =>
-  await Promise.all([
-    (console.log("API Call 1", performance.now()), fetchMajors()),
-    (console.log("API Call 2", performance.now()), fetchLiberalArts()),
-  ]);
+const fetchAllLectures = async () => {
+  const majors = (allLecturesCache.majors ??= fetchMajors());
+  const liberal = (allLecturesCache.liberal ??= fetchLiberalArts());
+  return Promise.all([majors, liberal]);
+};
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
