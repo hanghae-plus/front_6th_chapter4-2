aedc265dbf0d17f1af72389e776f0f9e4f07441c
areumH <dkfmal0109@gmail.com>
2025-09-10 23:30:29 +0900

    chore: 배포 환경 설정

--- src/api/lecture.ts
+++ src/api/lecture.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { Lecture } from '../types';
 
-export const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json'); // 전공 불러오기
-export const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.json'); // 교양 불러오기
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/front_6th_chapter4-2' : '';
+
+export const fetchMajors = () => axios.get<Lecture[]>(`${BASE_URL}/schedules-majors.json`); // 전공 불러오기
+export const fetchLiberalArts = () => axios.get<Lecture[]>(`${BASE_URL}/schedules-liberal-arts.json`); // 교양 불러오기
--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts',
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ['lcov', 'json', 'json-summary']
-      },
-    },
-  })
-)
+	defineConfig({
+		base: process.env.NODE_ENV === 'production' ? '/front_6th_chapter4-2/' : '/',
+		plugins: [react()],
+	}),
+	defineTestConfig({
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: './src/setupTests.ts',
+			coverage: {
+				reportsDirectory: './.coverage',
+				reporter: ['lcov', 'json', 'json-summary'],
+			},
+		},
+	})
+);
