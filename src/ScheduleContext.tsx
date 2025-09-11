371c3df34e287028110b2dbd29f544732194073a
현지수 <hyunzsu@naver.com>
2025-09-12 00:24:50 +0900

    refactor: useState에서 useReducer로 상태 관리 방식 변경

--- src/ScheduleContext.tsx
+++ src/ScheduleContext.tsx
@@ -2,36 +2,61 @@ import React, {
   createContext,
   PropsWithChildren,
   useContext,
-  useState,
-} from "react";
-import { Schedule } from "./types.ts";
-import dummyScheduleMap from "./dummyScheduleMap.ts";
+  useReducer,
+} from 'react';
+import { Schedule } from './types';
+import dummyScheduleMap from './dummyScheduleMap';
+import { scheduleReducer } from './reducers/scheduleReducer';
+import {
+  ScheduleActionsReturn,
+  useScheduleActions,
+} from './hooks/useScheduleActions';
 
 interface ScheduleContextType {
   schedulesMap: Record<string, Schedule[]>;
+  actions: ScheduleActionsReturn;
+  // 레거시 지원을 위한 setter (점진적 마이그레이션용)
   setSchedulesMap: React.Dispatch<
     React.SetStateAction<Record<string, Schedule[]>>
   >;
 }
 
 const ScheduleContext = createContext<ScheduleContextType | undefined>(
-  undefined,
+  undefined
 );
+
 
 export const useScheduleContext = () => {
   const context = useContext(ScheduleContext);
   if (context === undefined) {
-    throw new Error("useSchedule must be used within a ScheduleProvider");
+    throw new Error('useSchedule must be used within a ScheduleProvider');
   }
   return context;
 };
 
 export const ScheduleProvider = ({ children }: PropsWithChildren) => {
-  const [schedulesMap, setSchedulesMap] =
-    useState<Record<string, Schedule[]>>(dummyScheduleMap);
+  const [schedulesMap, dispatch] = useReducer(
+    scheduleReducer,
+    dummyScheduleMap
+  );
+  const actions = useScheduleActions(dispatch);
+
+  // 레거시 지원을 위한 setter
+  const setSchedulesMap = (
+    update: React.SetStateAction<Record<string, Schedule[]>>
+  ) => {
+    if (typeof update === 'function') {
+      const newMap = update(schedulesMap);
+      dispatch({ type: 'SET_SCHEDULES_MAP', schedulesMap: newMap });
+    } else {
+      dispatch({ type: 'SET_SCHEDULES_MAP', schedulesMap: update });
+    }
+  };
 
   return (
-    <ScheduleContext.Provider value={{ schedulesMap, setSchedulesMap }}>
+    <ScheduleContext.Provider
+      value={{ schedulesMap, actions, setSchedulesMap }}
+    >
       {children}
     </ScheduleContext.Provider>
   );
