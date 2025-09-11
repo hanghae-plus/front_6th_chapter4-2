95f9bde4263481016ed34137fa7d02ba1d779386
HYOJIN <hyo.khj@gmail.com>
2025-09-12 05:35:17 +0900

    배포 설정

--- src/ScheduleProvider.tsx
+++ src/ScheduleProvider.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import { ScheduleStateProvider } from "./context/ScheduleStateContext.tsx";
 import { ScheduleActionsProvider } from "./context/ScheduleActionsContext.tsx";
 
