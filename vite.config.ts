d4e913474a4c50231e858f64d026c2b788daea93
jihyeon <rachel2148072@gmail.com>
2025-09-12 03:11:51 +0900

    build: 빌드 및 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+
+const base =
+  process.env.NODE_ENV === "production" ? "/front_6th_chapter4-2/" : "";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base,
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          404: resolve(__dirname, "404.html"),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts',
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
       coverage: {
         reportsDirectory: "./.coverage",
-        reporter: ['lcov', 'json', 'json-summary']
+        reporter: ["lcov", "json", "json-summary"],
       },
     },
   })
-)
+);
