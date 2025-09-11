ee7a24a4f37af5feb7c6b39feb32286f65fddd30
q1Lim <cochlea00@gmail.com>
2025-09-11 23:23:39 +0900

    feat: 강의 Filter 컴포넌트 분리 - Credit - Day - Grade - Major - PeriodTime - Query

---
+++ src/DayFilter.tsx
@@ -0,0 +1,29 @@
+import { Checkbox, CheckboxGroup, FormControl, FormLabel, HStack } from "@chakra-ui/react";
+import { DAY_LABELS } from "./constants.ts";
+import { SearchOption } from "./types.ts";
+import { memo } from "react";
+
+type DayFilterProps = {
+    days: SearchOption['days'];
+    changeSearchOption: (field: keyof SearchOption, value: SearchOption[typeof field]) => void;
+}
+
+const DayFilter = ({ days, changeSearchOption }: DayFilterProps) => {
+    return (
+        <FormControl>
+            <FormLabel>요일</FormLabel>
+            <CheckboxGroup
+                value={days}
+                onChange={(value) => changeSearchOption('days', value as string[])}
+            >
+                <HStack spacing={4}>
+                    {DAY_LABELS.map(day => (
+                        <Checkbox key={day} value={day}>{day}</Checkbox>
+                    ))}
+                </HStack>
+            </CheckboxGroup>
+        </FormControl>
+    )
+}
+
+export default memo(DayFilter);
