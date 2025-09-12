45a025d9e81dedba3d97d2c931fa9be79f69dcfc
eveneul <nebulas9724@gmail.com>
2025-09-12 11:06:24 +0900

    fix: vite 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import {defineConfig as defineTestConfig, mergeConfig} from 'vitest/config'
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-    base: "/front_6th_chapter4-2/",
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
-      },
-    },
-  })
-);
+	defineConfig({
+		plugins: [react()],
+		base: '/.'
+	}),
+	defineTestConfig({
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: './src/setupTests.ts',
+			coverage: {
+				reportsDirectory: './.coverage',
+				reporter: ['lcov', 'json', 'json-summary']
+			}
+		}
+	})
+)
